563cfdd567b3c7531656e5ee8f4e3fda44436aa0
ANDANI EMMANUEL LOPEZ ARECHAR <andani.lopez@grupobimbo.com>
2026-07-14 15:51:20 -0600

    Fix: Increase map window size from 350x500 to 700x600 to display plant layout completely

--- Wall_E_coordinator_raspberry.py
+++ Wall_E_coordinator_raspberry.py
@@ -1131,7 +1131,7 @@ class AppIndustrial:
         # Create a new window for the map
         top = tk.Toplevel(self.root)
         top.title("Plant Map - Wall-E")
-        top.geometry("350x500")
+        top.geometry("700x600")
         top.configure(bg="#222222")
         top.resizable(True, True)
         
